c79fef396d7a9ad3f476b2332502d4d866051452
Souza <130477890+dank-gui@users.noreply.github.com>
2024-10-04 15:17:29 +0000

    Rota home e sobre. Iiniciado o menu

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-var http = require("http");
+const express = require("express");
+const app = express();
 
-//create a server object:
-http
-  .createServer(function (req, res) {
-    res.write("Hello from CodeSandbox!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+const PORT = 3301;
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/src/views/index.html");
+});
+
+app.get("/sobre", function (req, res) {
+  res.sendFile(__dirname + "/src/views/about.html");
+});
+//ROTA UTILIZANDO MIDDLEWARE(404)
+app.use(function (req, res) {
+  res.status(404).sendFile(__dirname + "/src/views/404.html");
+});
+app.listen(PORT, () => {
+  console.log("running...");
+});
